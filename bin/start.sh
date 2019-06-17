873ebd625f989623d66db3a061dd38413b784538
piginzoo <piginzoo@gmail.com>
2019-06-17 19:21:37 +0800

    update

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,4 @@
 
  #python split_image.py [src_dir] [dst_dir] <label_name>(可选,label.txt)
- nohup python -u  data_generator/split_image.py $1 $2 $3 ./logs/split.log 2>&1 &
+ #nohup python -u  data_generator/split_image.py $1 $2 $3 ./logs/split.log 2>&1 &
+ python -u  data_generator/split_image.py $1 $2 $3
